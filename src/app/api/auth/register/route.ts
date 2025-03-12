0b1628db10cb0a3af4942ceef6a07b4d0dbb5f00
PIEROLS15 <pierodanielllanossanchez@gmail.com>
2025-03-12 01:00:43 -0500

    fix(Middleware): Se eliminó el middleware

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,9 +1,8 @@
 import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import { PrismaClient } from "@prisma/client";
+import prisma from "@/lib/prisma";
 
-const prisma = new PrismaClient();
 const SECRET_KEY = process.env.JWT_SECRET || "super_secreto";
 
 export async function POST(req: Request) {
